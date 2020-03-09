0bca447a5c7a7f62692d364ff5f0f712417ea3f0
sys <sys@qq.com>
2020-03-09 17:32:33 +0800

    Initial commit

---
+++ src/main/java/cn/blibli/bli/BliApplication.java
@@ -0,0 +1,13 @@
+package cn.blibli.bli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BliApplication.class, args);
+    }
+
+}
